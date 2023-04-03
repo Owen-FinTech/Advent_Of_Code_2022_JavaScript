f900ab0d87fb08e2bbe3f0d31d0219e74701476e
Owen Harris <harrisowen776@gmail.com>
2023-04-03 15:08:47 +1000

    Added day 4 pt 1 & pt 2

---
+++ advent_of_code_22_day_4_pt_1.js
@@ -0,0 +1,28 @@
+let inputPairs = ``; // Insert your puzzle input between the backticks
+
+let inputArray = inputPairs.split('\n');
+
+let pairsArray = [];
+for (let i = 0; i < inputArray.length; i++) {
+    let splitArray = inputArray[i].split(',');
+    pairsArray.push(splitArray[0]);
+    pairsArray.push(splitArray[1]);
+}
+
+let singleArray = [];
+for (let i = 0; i < pairsArray.length; i++) {
+    let singleSplit = pairsArray[i].split('-');
+    singleArray.push(singleSplit[0]);
+    singleArray.push(singleSplit[1]);
+}
+
+let total = 0;
+for (let i = 0; i < singleArray.length; i += 4) {
+    if (Number(singleArray[i]) <= Number(singleArray[i + 2]) && Number(singleArray[i + 1]) >= Number(singleArray[i + 3])) {
+        total += 1;
+    } else if (Number(singleArray[i]) >= Number(singleArray[i + 2]) && Number(singleArray[i + 1]) <= Number(singleArray[i + 3])) {
+        total += 1;
+    }
+}
+
+console.log("Total: ", total);
